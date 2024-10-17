716d8a65d026238fca474cc7e3f3495974104239
Stephan Johansen <stephan.johansen@outlook.com>
2024-10-17 22:12:01 +0200

    chore: Base dotnet projects

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kata
+
+Url for Kata:
---
+++ tests/Kata.Logic.Test/KataTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Logic.Test;
+
+public class KataTests
+{
+    [Fact]
+    public void MethodName_StateUnderTest_ExpectedBehavior1()
+    {
+        // Arrange
+        var sut = new Kata();
+        
+        // Assert
+    }
+
+    [Fact]
+    public void MethodName_StateUnderTest_ExpectedBehavior2()
+    {
+        // Arrange
+        // Act
+        // Assert
+ 
+    }
+
+    
+}
