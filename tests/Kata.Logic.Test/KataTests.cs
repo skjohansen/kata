ffc31dcb7694415b6da88d0c5865a4025f2b4042
Stephan Johansen <stephan.johansen@outlook.com>
2024-12-15 13:15:59 +0100

    chore: Test output

--- tests/Kata.Logic.Test/KataTests.cs
+++ tests/Kata.Logic.Test/KataTests.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit.Abstractions;
 
 namespace TicTacToe.Logic.Test;
 
-public class KataTests
+public class KataTests(ITestOutputHelper output)
 {
     [Fact]
     public void MethodName_StateUnderTest_ExpectedBehavior1()
     {
+        output.WriteLine("Test steps");
+
         // Arrange
         var sut = new Kata();
         
         // Assert
     }
 
-    [Fact]
+    [Fact(DisplayName = "Pretty name"]
     public void MethodName_StateUnderTest_ExpectedBehavior2()
     {
         // Arrange
